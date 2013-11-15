d6bddda896f26c2f2e347d36aecb379bbd0274b8
David Su <dtscraft@gmail.com>
2013-11-14 19:42:15 -0800

    initial rails app

---
+++ calcentral_events/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalcentralEvents::Application.config.secret_token = '3b71078a5ccb1665c496fae0c66645bca4a0ae4564c21f0bd5e7d6fedc825dac002551cc5959c8577ca9063f60bc060d961e75ff37b23279a520c7dba8d2fa60'
