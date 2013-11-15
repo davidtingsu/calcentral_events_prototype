1c52457f298bd46c74d4707b5f6f4b906c561168
David Su <dtscraft@gmail.com>
2013-11-15 13:12:19 -0800

    basic search for clubs and categories

--- calcentral_events/app/controllers/events_controller.rb
+++ calcentral_events/app/controllers/events_controller.rb
@@ -3,5 +3,12 @@ class EventsController < ApplicationController
   end
 
   def search
+        @events = Event.reverse_chronological_order.all
+        if params[:category].present?
+            @events = Event.find_by_category(*params[:category].split(','))
+        end
+        if params[:club].present?
+            @events = Event.find_by_club(*params[:club].split(','))
+        end
   end
 end
