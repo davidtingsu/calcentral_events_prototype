e4996187bc236422e222d168a1f2a41037d19bf6
David Su <dtscraft@gmail.com>
2013-12-03 12:59:02 -0800

    helper methods for a club to get facebook events
    Currently only supports club with facebook pages
    Also add scopes to query clubs with facebook_id, facebook_url, or
    callink_id

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,6 +2,37 @@ class Club < ActiveRecord::Base
   has_many :events 
   attr_accessible :description, :name, :facebook_id, :facebook_url, :callink_id, :callink_permalink
   has_many :categories
+  scope :with_facebook_url, ->(){ where("facebook_url IS NOT NULL") }
+  scope :facebook, ->(){ where("facebook_id IS NOT NULL") }
+  scope :callink, ->(){ where("callink_id IS NOT NULL") }
+
+  def is_facebook?
+    facebook_id.present?
+  end
+
+  def set_facebook_id!
+    if facebook_url.present?
+      id = Event.facebookIDFromFacebookPageUrl(facebook_url)
+      self.update_attribute(:facebook_id, id) if /^\d+$/.match(id)
+    end
+  end
+
+  def is_callink?
+    callink_id.present?
+  end
+
+  def update_events!
+    # gets and creates association with for facebook page events
+    # TODO: get events for facebook groups when user access token present
+    # There is currently no direct way a callink clubs
+    if is_facebook?
+        events = Event.getFacebookEvents(facebook_id)
+        events.each{ |event_hash|
+            update_or_create_facebook_event(event_hash)
+        }
+    end
+  end
+
   def get_facebook_group_events(user_access_token)
     MiniFB.get(user_access_token, facebook_id , :type => "events") if (facebook_id and user_access_token != " ")
   end
@@ -51,4 +82,10 @@ class Club < ActiveRecord::Base
 
     club
   end
+  def update_or_create_facebook_event(event_hash, only_future = true)
+    event ||= Event.find_by_facebook_id(event_hash.eid)
+    event ||= Event.new(:name => event_hash.name, :start_time => event_hash.start_time, :end_time => event_hash.end_time, :description => event_hash.description)
+    events << event and save! if (only_future and event.start_time.future?) or ! only_future
+  end
+
 end
